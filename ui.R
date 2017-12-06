922ea46642538d13a4d4d778791dccd89c4b7827
smilejin92 <smilejin92@gmail.com>
2017-12-06 09:45:05 -0800

    changed introduction

--- ui.R
+++ ui.R
@@ -29,10 +29,10 @@ ui <- fluidPage(
                 The purpose of this project is to give the scholarly audiences perception of how certain race populations
                 have a higher or lower probability of higher or lower birthrate while factoring in education, income and geographic 
                 location through the art of data science. In this analysis, we implemented four maps using R Studio, which showed us the 
-                statistics of birthrate by state per year, income versus birthrate analysis based on income per year for every state, 
-                the changes of birth rate per year for every state, race populations on average over the last five years, and the education
-                attainment per year for every state.This project was designed by a group of four innovative informatic students who are passionate 
-                to make a difference in a world through data science. We believe that through this project on birthrates statistics and fields 
+                statistics of child-woman ratio by state per year, income versus child-woman ratio analysis based on income per year for every state, 
+                the changes of child-woman ratio per year for every state, race populations on average over the last five years, and the education
+                attainment per year for every state.This project was designed by a group of four innovative Informatics students who are passionate 
+                to make a difference in a world through data science. We believe that through this project on birthrate statistics and fields 
                 such as race populations, geographic location and  education and  in the US will allow scholarly audiences to learn, analyze,
                 and apply the knowledge extracted from big data.")
           )
