df03e505bd727cffdbabcf14a84256cdfc38c5db
jskazn1 <32310374+jskazn1@users.noreply.github.com>
2017-12-06 09:54:41 -0800

    Fixed word names on state analysis list

--- ui.R
+++ ui.R
@@ -59,20 +59,20 @@ ui <- fluidPage(
         sidebarLayout(
           sidebarPanel (
             selectInput("state", "Choose State",
-                        choices = list(Alabama = 'Alabama', Alaska = 'Alaska', Arizona = 'Arizona', Arkansas = 'Arkansas',
-                                       California = 'California', Colorado = 'Colorado', Connecticut = 'Connecticut',
-                                       Delaware = 'Delaware', DistrictOfColumbia = 'District of Columbia', Florida = 'Florida',
-                                       Georgia = 'Georgia', Hawaii = 'Hawaii', Idaho = 'Idaho', Illinois = 'Illinois',
-                                       Indiana = 'Indiana', Iowa = 'Iowa', Kansas = 'Kansas', Kentucky = 'Kentucky',
-                                       Louisiana = 'Louisiana', Maine = 'Maine', Maryland = 'Maryland', Massachusetts = 'Massachusetts',
-                                       Michigan = 'Michigan', Minnesota = 'Minnesota', Mississippi = 'Mississippi', Missouri = 'Missouri',
-                                       Montana = 'Montana', Nebraska = 'Nebraska', Nevada = 'Nevada',
-                                       NewHampshire = 'New Hampshire', NewJersey = 'New Jersey', NewMexico = 'New Mexico',
-                                       NewYork = 'New York', NorthCarolina = 'North Carolina', NorthDakota = 'North Dakota',
-                                       Ohio = 'Ohio', Oklahoma = 'Oklahoma', Oregon = 'Oregon', Pennsylvania = 'Pennsylvania',
-                                       RhodeIsland = 'Rhode Island', SouthCarolina = 'South Carolina', SouthDakota = 'South Dakota',
-                                       Tennessee = 'Tennessee', Texas = 'Texas', Utah = 'Utah', Vermont = 'Vermont', Virginia = 'Virginia',
-                                       Washington = 'Washington', WestVirginia = 'West Virginia', Wisconsin = 'Wisconsin', Wyoming = 'Wyoming')
+                        choices = list('Alabama' = 'Alabama', 'Alaska' = 'Alaska', 'Arizona' = 'Arizona', 'Arkansas' = 'Arkansas',
+                                       'California' = 'California', 'Colorado' = 'Colorado', 'Connecticut' = 'Connecticut',
+                                       'Delaware' = 'Delaware', 'District Of Columbia' = 'District of Columbia', 'Florida' = 'Florida',
+                                       'Georgia' = 'Georgia', 'Hawaii' = 'Hawaii', 'Idaho' = 'Idaho', 'Illinois' = 'Illinois',
+                                       'Indiana' = 'Indiana', 'Iowa' = 'Iowa', 'Kansas' = 'Kansas', 'Kentucky' = 'Kentucky',
+                                       'Louisiana' = 'Louisiana', 'Maine' = 'Maine', 'Maryland' = 'Maryland', 'Massachusetts' = 'Massachusetts',
+                                       'Michigan' = 'Michigan', 'Minnesota' = 'Minnesota', 'Mississippi' = 'Mississippi', 'Missouri' = 'Missouri',
+                                       'Montana' = 'Montana', 'Nebraska' = 'Nebraska', 'Nevada' = 'Nevada',
+                                       'New Hampshire' = 'New Hampshire', 'New Jersey' = 'New Jersey', 'New Mexico' = 'New Mexico',
+                                       'New York' = 'New York', 'North Carolina' = 'North Carolina', 'North Dakota' = 'North Dakota',
+                                       'Ohio' = 'Ohio', 'Oklahoma' = 'Oklahoma', 'Oregon' = 'Oregon', 'Pennsylvania' = 'Pennsylvania',
+                                       'Rhode Island' = 'Rhode Island', 'South Carolina' = 'South Carolina', 'South Dakota' = 'South Dakota',
+                                       'Tennessee' = 'Tennessee', 'Texas' = 'Texas', 'Utah' = 'Utah', 'Vermont' = 'Vermont', 'Virginia' = 'Virginia',
+                                       'Washington' = 'Washington', 'West Virginia' = 'West Virginia', 'Wisconsin' = 'Wisconsin', 'Wyoming' = 'Wyoming')
             ),
             radioButtons(
               "sort.by", "Sort by", c("Race", "Education")
