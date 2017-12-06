94f0d247fdb8faf360f69b16ce59f3a675ac16bf
aksor7 <aksor7@gmail.com>
2017-12-06 09:36:42 -0800

    Updated Introduction Paragraph in ui.R

--- ui.R
+++ ui.R
@@ -27,12 +27,13 @@ ui <- fluidPage(
             
             h4("Our team has designed four analytical maps that show the change of birthrate overtime within the last five years from 2012 to 2016.
                 The purpose of this project is to give the scholarly audiences perception of how certain race populations
-                have a higher or lower probability of higher or lower birthrate while factoring in education, income and geographic location through the art of data science. 
-                In this analysis, we implemented four maps using R Studio, which showed us the statistics of birthrate by state per year, income versus birthrate
-                analysis based on income per year for every state, the changes of birth rate per year for every state and the five year average for the race populations.
-                This project was designed by a group of four innovative informatic students who are passionate to make a difference in a world through data science.
-                We believe that through this project on birthrates statistics and fields such as race populations,
-                geographic location and  education and  in the US will allow scholarly audiences to learn, analyze,
+                have a higher or lower probability of higher or lower birthrate while factoring in education, income and geographic 
+                location through the art of data science. In this analysis, we implemented four maps using R Studio, which showed us the 
+                statistics of birthrate by state per year, income versus birthrate analysis based on income per year for every state, 
+                the changes of birth rate per year for every state, race populations on average over the last five years, and the education
+                attainment per year for every state.This project was designed by a group of four innovative informatic students who are passionate 
+                to make a difference in a world through data science. We believe that through this project on birthrates statistics and fields 
+                such as race populations, geographic location and  education and  in the US will allow scholarly audiences to learn, analyze,
                 and apply the knowledge extracted from big data.")
           )
         )
