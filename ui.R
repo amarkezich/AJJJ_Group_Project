49fad7844a84fe8de72224f986935f98e153b472
jskazn1 <32310374+jskazn1@users.noreply.github.com>
2017-12-06 10:02:29 -0800

    Fixed wording on paragraph

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ source("babyplot.R")
 
 ui <- fluidPage(
   # Title
-  titlePanel("Child-Woman Ratio Analysis"),
+  titlePanel("Child-Woman Ratio (Birthrate) Analysis"),
   mainPanel( # decides what goes into main panel
     
     tabsetPanel(
@@ -27,13 +27,13 @@ ui <- fluidPage(
             
             h4("Our team has designed four analytical maps that show the change of birthrate overtime within the last five years from 2012 to 2016.
                 The purpose of this project is to give the scholarly audiences perception of how certain race populations
-                have a higher or lower probability of higher or lower birthrate while factoring in education, income and geographic 
-                location through the art of data science. In this analysis, we implemented four maps using R Studio, which showed us the 
+                have a higher or lower probability of higher or lower birthrates while factoring in education, income and geographic 
+                location through the art of data science. In this analysis, we implemented four maps using RStudio, which showed us the 
                 statistics of child-woman ratio by state per year, income versus child-woman ratio analysis based on income per year for every state, 
                 the changes of child-woman ratio per year for every state, race populations on average over the last five years, and the education
-                attainment per year for every state.This project was designed by a group of four innovative Informatics students who are passionate 
-                to make a difference in a world through data science. We believe that through this project on birthrate statistics and fields 
-                such as race populations, geographic location and  education and  in the US will allow scholarly audiences to learn, analyze,
+                attainment per year for every state. This project was designed by a group of four innovative Informatics students who are passionate 
+                to make a difference in the world through data science. We believe that through this project on birthrate statistics and fields 
+                such as race populations, geographic location and education and in the US will allow scholarly audiences to learn, analyze,
                 and apply the knowledge extracted from big data.")
           )
         )
